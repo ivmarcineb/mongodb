d18199fb563470713fee838857ba0c664dcd1ed0
Ivan Marcin <ivan@elasticbox.com>
2014-05-16 12:35:17 -0700

    Mongo renamed package to mongodb-org

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,8 +5,8 @@ class mongodb::params() {
   
   case $::operatingsystem {
     /(Amazon|CentOS|Fedora|RedHat)/: {
-      $mongo_10gen        = 'mongo-10gen'
-      $mongo_10gen_server = 'mongo-10gen-server'
+      $mongo_10gen        = 'mongodb-org'
+      $mongo_10gen_server = 'mongodb-org-server'
       $mongo_user         = 'mongod'
       $mongo_service      = 'mongod'
       $mongo_config       = 'mongod.conf'
@@ -14,7 +14,7 @@ class mongodb::params() {
       $mongo_path         = '/var/lib/mongo'
     }
     /(Debian|Ubuntu)/: {
-      $mongo_10gen        = 'mongodb-10gen'
+      $mongo_10gen        = 'mongodb-org'
       $mongo_10gen_server = undef
       $mongo_user         = 'mongodb'
       $mongo_service      = 'mongodb'
